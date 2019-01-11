1494acf886fe4c9fbb261fb79504aaed4958b091
Harry Westbrook <up785080@myport.ac.uk>
2019-01-11 11:38:46 +0000

    Weather Script & Initial Data Gathering
    Initial data gathering for weather and course/unit data. Script included for calling and automating daily weather API.

---
+++ weather_api_script.ps1
@@ -0,0 +1,8 @@
+$date1 = [DateTime]::Today.AddDays(-1)
+$date2 = $date1.ToString("yyyy-MM-dd")
+$date3 = $date1.ToString("ddMMyyyy")
+
+
+$url = "http://api.apixu.com/v1/history.json?key=01947a9e53734216889100359190801&q=Portsmouth, UK&dt=$date2"
+
+Invoke-RestMethod -Uri $url -OutFile "C:\Users\Harry\Documents\University\Year 3\Dissertation\UoP_Dissertation\data\weather_data\weather_data_$date3.json"
