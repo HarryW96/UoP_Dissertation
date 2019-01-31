ebfc80a1b650b47c3b4b26323ce40b01d3980de8
Harry Westbrook <ig3nharry@gmail.com>
2019-01-31 15:13:11 +0000

    Tested and Update Table_Build
    Tested that Table_Build.sql worked correctly and adjusted where needed.

--- sql/table_build.sql
+++ sql/table_build.sql
@@ -1,16 +1,16 @@
 -- File used to build the tables for UoP dissertation. 
 CREATE TABLE Course(
-    course_id VARCHAR2 UNIQUE NOT NULL,
-    course_name VARCHAR2 NOT NULL,
-    study_mode VARCHAR2 NOT NULL,
-    academic_year VARCHAR2 NOT NULL,
+    course_id VARCHAR UNIQUE NOT NULL,
+    course_name VARCHAR NOT NULL,
+    study_mode VARCHAR NOT NULL,
+    academic_year VARCHAR NOT NULL,
     PRIMARY KEY (course_id)
 );
 
 CREATE TABLE Student(
-    student_id VARCHAR2 UNIQUE NOT NULL,
-    course_id VARCHAR2 NOT NULL,
-    transport_method VARCHAR2 NOT NULL,
+    student_id VARCHAR UNIQUE NOT NULL,
+    course_id VARCHAR NOT NULL,
+    transport_method VARCHAR NOT NULL,
     PRIMARY KEY (student_id),
     CONSTRAINT student_course_id_fkey FOREIGN KEY (course_id)
         REFERENCES Course (course_id)
@@ -18,18 +18,18 @@ CREATE TABLE Student(
 );
 
 CREATE TABLE Lecturer(
-    lecturer_id VARCHAR2 UNIQUE NOT NULL,
-    lecturer_name VARCHAR2 NOT NULL,
-    email_address VARCHAR2 NOT NULL,
-    contact_no VARCHAR2,
+    lecturer_id VARCHAR UNIQUE NOT NULL,
+    lecturer_name VARCHAR NOT NULL,
+    email_address VARCHAR NOT NULL,
+    contact_no VARCHAR,
     PRIMARY KEY (lecturer_id)
 );
 
 CREATE TABLE Unit(
-    unit_code VARCHAR2 UNIQUE NOT NULL,
-    unit_coord VARCHAR2 NOT NULL,
-    unit_name VARCHAR2 NOT NULL,
-    short_name VARCHAR2 NOT NULL,
+    unit_code VARCHAR UNIQUE NOT NULL,
+    unit_coord VARCHAR NOT NULL,
+    unit_name VARCHAR NOT NULL,
+    short_name VARCHAR NOT NULL,
     credits INTEGER NOT NULL,
     study_level INTEGER NOT NULL,
     PRIMARY KEY (unit_code),
@@ -39,10 +39,10 @@ CREATE TABLE Unit(
 );
 
 CREATE TABLE Course_unit(
-    course_id VARCHAR2 UNIQUE NOT NULL,
-    unit_code VARCHAR2 NOT NULL,
+    course_id VARCHAR UNIQUE NOT NULL,
+    unit_code VARCHAR NOT NULL,
     optional BOOLEAN NOT NULL,
-    PRIMARY KEY (course_id, role_id),
+    PRIMARY KEY (course_id),
     CONSTRAINT course_unit_course_id_fkey FOREIGN KEY (course_id)
         REFERENCES Course (course_id)
         ON UPDATE CASCADE ON DELETE NO ACTION,
@@ -53,30 +53,30 @@ CREATE TABLE Course_unit(
 
 CREATE TABLE Building(
     building_id SERIAL NOT NULL,
-    building_name VARCHAR2 NOT NULL,
-    street_address1 VARCHAR2,
-    street_address2 VARCHAR2,
-    city VARCHAR2,
-    county VARCHAR2,
-    postcode VARCHAR2,
+    building_name VARCHAR NOT NULL,
+    street_address1 VARCHAR,
+    street_address2 VARCHAR,
+    city VARCHAR,
+    county VARCHAR,
+    postcode VARCHAR,
     PRIMARY KEY (building_id)
 );
 
 CREATE TABLE Room(
-    room_code VARCHAR2 UNIQUE NOT NULL,
-    bui_id VARCHAR2 NOT NULL,
-    room_no VARCHAR2 NOT NULL,
+    room_code VARCHAR UNIQUE NOT NULL,
+    building_id SERIAL NOT NULL,
+    room_no VARCHAR NOT NULL,
     PRIMARY KEY (room_code),
-    CONSTRAINT room_bui_id_fkey FOREIGN KEY (bui_id)
-        REFERENCES building (bui_id)
+    CONSTRAINT room_bui_id_fkey FOREIGN KEY (building_id)
+        REFERENCES building (building_id)
         ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE Lecture(
     lecture_id SERIAL NOT NULL,
-    unit_code VARCHAR2 NOT NULL,
-    room_id VARCHAR2 NOT NULL,
-    lecturer_id VARCHAR2 NOT NULL,
+    unit_code VARCHAR NOT NULL,
+    room_code VARCHAR NOT NULL,
+    lecturer_id VARCHAR NOT NULL,
     -- lecture_date_time ??
     lecture_time TIME WITH TIME ZONE NOT NULL,
     lecture_date DATE NOT NULL,
@@ -84,8 +84,8 @@ CREATE TABLE Lecture(
     CONSTRAINT lecture_unit_code_fkey FOREIGN KEY(unit_code)
         REFERENCES Unit (unit_code)
         ON UPDATE CASCADE ON DELETE NO ACTION,
-    CONSTRAINT lecture_room_id_fkey FOREIGN KEY(room_id)
-        REFERENCES Room (room_id)
+    CONSTRAINT lecture_room_id_fkey FOREIGN KEY(room_code)
+        REFERENCES Room (room_code)
         ON UPDATE CASCADE ON DELETE CASCADE,
     CONSTRAINT lecture_lecturer_id_fkey FOREIGN KEY(lecturer_id)
         REFERENCES Lecturer (lecturer_id)
@@ -93,7 +93,7 @@ CREATE TABLE Lecture(
 );
 
 CREATE TABLE Attendance(
-    student_id VARCHAR2 NOT NULL,
+    student_id VARCHAR NOT NULL,
     lecture_id SERIAL NOT NULL,
     attended BOOLEAN NOT NULL,
     PRIMARY KEY (student_id, lecture_id),
@@ -105,17 +105,17 @@ CREATE TABLE Attendance(
         ON UPDATE NO ACTION ON DELETE NO ACTION
 );
 
-CREATE TABLE Accomodation(
-    accomodation_id SERIAL NOT NULL,
-    student_id VARCHAR2 NOT NULL,
-    room_no VARCHAR2,
-    building_name VARCHAR2 NOT NULL,
-    street_address1 VARCHAR2 NOT NULL,
-    street_address2 VARCHAR2 NOT NULL,
-    city VARCHAR2 NOT NULL,
-    county VARCHAR2 NOT NULL,
-    postcode VARCHAR2 NOT NULL,
-    PRIMARY KEY (acommodation_id),
+CREATE TABLE Accommodation(
+    accommodation_id SERIAL NOT NULL,
+    student_id VARCHAR NOT NULL,
+    room_no VARCHAR,
+    building_name VARCHAR NOT NULL,
+    street_address1 VARCHAR NOT NULL,
+    street_address2 VARCHAR NOT NULL,
+    city VARCHAR NOT NULL,
+    county VARCHAR NOT NULL,
+    postcode VARCHAR NOT NULL,
+    PRIMARY KEY (accommodation_id),
     CONSTRAINT accomodation_student_id_fkey FOREIGN KEY(student_id)
         REFERENCES Student (student_id)
         ON UPDATE CASCADE ON DELETE NO ACTION
@@ -123,25 +123,25 @@ CREATE TABLE Accomodation(
 
 CREATE TABLE Travel_Time(
     travel_id SERIAL NOT NULL,
-    accom_id VARCHAR2 NOT NULL,
-    bui_id VARCHAR2 NOT NULL,
+    accommodation_id SERIAL NOT NULL,
+    building_id SERIAL NOT NULL,
     time_of_travel TIME NOT NULL,
     walk_time TIME,
     drive_time TIME,
     transit_time TIME,
     cycle_time TIME,
     PRIMARY KEY (travel_id),
-    CONSTRAINT travel_time_accom_id_fkey FOREIGN KEY(accom_id)
-        REFERENCES Accomodation (accomodation_id)
+    CONSTRAINT travel_time_accom_id_fkey FOREIGN KEY(accommodation_id)
+        REFERENCES Accommodation (accommodation_id)
         ON UPDATE CASCADE ON DELETE NO ACTION,
-    CONSTRAINT travel_time_bui_id_fkey FOREIGN KEY(bui_id)
+    CONSTRAINT travel_time_bui_id_fkey FOREIGN KEY(building_id)
         REFERENCES Building (building_id)
         ON UPDATE CASCADE ON DELETE NO ACTION 
 );
 
 -- Come back to, build 2 weather tables from JSON & CSV and then collate into single table.
 CREATE TABLE Weather(
-    weather_date DATETIME UNIQUE NOT NULL,
+    weather_date TIMESTAMP UNIQUE NOT NULL,
     temp_c FLOAT NOT NULL,
     humidity INT NOT NULL,
     wind_speed INT NOT NULL,
@@ -152,9 +152,9 @@ CREATE TABLE Weather(
 
 CREATE TABLE City_Event(
     event_id SERIAL NOT NULL,
-    event_name VARCHAR2 NOT NULL,
-    start_datetime DATETIME NOT NULL,
-    end_datetime DATETIME NOT NULL,
+    event_name VARCHAR NOT NULL,
+    start_datetime TIMESTAMP NOT NULL,
+    end_datetime TIMESTAMP NOT NULL,
     PRIMARY KEY (event_id)
 );
 
